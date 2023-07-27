e62b95a1efcfd948d4ab0ae5d0652c6d595103c5
Dajuuu <40289712+Dajuuu@users.noreply.github.com>
2023-07-27 14:20:39 +0200

    Implemented timer

--- RestScreen.js
+++ RestScreen.js
@@ -1,20 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 
 const RestScreen = ({ navigation }) => {
+  const [timer, setTimer] = useState(45);
+  const [isPaused, setIsPaused] = useState(false);
+
+  useEffect(() => {
+    // Start the timer when the screen mounts
+    const interval = setInterval(() => {
+      if (!isPaused && timer > 0) {
+        setTimer((prevTimer) => prevTimer - 1);
+      }
+    }, 1000);
+
+    // Clear the interval when the screen unmounts
+    return () => clearInterval(interval);
+  }, [isPaused, timer]);
+
   const handleSkip = () => {
     // Navigate to the next screen after rest (you can replace "NextScreen" with your desired screen name)
     navigation.navigate("NextScreen");
   };
 
+  const handlePauseResume = () => {
+    setIsPaused((prevIsPaused) => !prevIsPaused);
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Take a rest</Text>
+      <Text style={styles.timer}>{timer} seconds</Text>
       <TouchableOpacity style={styles.button} onPress={handleSkip}>
         <Text style={styles.buttonText}>Skip</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.button} onPress={() => {}}>
-        <Text style={styles.buttonText}>Pause</Text>
+      <TouchableOpacity style={styles.button} onPress={handlePauseResume}>
+        <Text style={styles.buttonText}>{isPaused ? "Resume" : "Pause"}</Text>
       </TouchableOpacity>
     </View>
   );
@@ -30,6 +50,10 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 24,
     fontWeight: "bold",
+    marginBottom: 20,
+  },
+  timer: {
+    fontSize: 18,
     marginBottom: 20,
   },
   button: {
