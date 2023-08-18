22602c8c32eacd113aa1168f130ab29ced771a44
Dajuuu <40289712+Dajuuu@users.noreply.github.com>
2023-08-18 17:32:29 +0200

    SelectedWorkout comments

--- SelectedWorkout.js
+++ SelectedWorkout.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useState, useEffect } from "react";
+import React from "react";
 import {
   View,
   Text,
@@ -16,80 +16,33 @@ const SelectedWorkoutScreen = ({ navigation, route }) => {
   const workoutDifficulties = [
     {
       difficulty: "Easy 1",
-      // screen: "EasyLevels",
-      // imageSource: require("fit-buddy/assets/HomeScreen/arms-workout.png"),
     },
     {
       difficulty: "Easy 2",
-      // screen: "EasyLevels",
-      // imageSource: require("fit-buddy/assets/HomeScreen/arms-workout.png"),
     },
     {
       difficulty: "Medium 1",
-      // screen: "EasyLevels",
-      // imageSource: require("fit-buddy/assets/HomeScreen/arms-workout.png"),
     },
     {
       difficulty: "Medium 2",
-      // screen: "EasyLevels",
-      // imageSource: require("fit-buddy/assets/HomeScreen/arms-workout.png"),
     },
     {
       difficulty: "Hard 1",
-      // screen: "EasyLevels",
-      // imageSource: require("fit-buddy/assets/HomeScreen/arms-workout.png"),
     },
     {
       difficulty: "Hard 2",
-      // screen: "EasyLevels",
-      // imageSource: require("fit-buddy/assets/HomeScreen/arms-workout.png"),
     },
-    // {
-    //   type: "Legs",
-    //   screen: "MediumLevels",
-    //   imageSource: require("fit-buddy/assets/HomeScreen/legs-workout.png"),
-    // },
-    // {
-    //   type: "ABS",
-    //   screen: "HardLevels",
-    //   imageSource: require("fit-buddy/assets/HomeScreen/abs-workout.jpg"),
-    // },
-    // {
-    //   type: "FBW",
-    //   screen: "ExpertLevels",
-    //   imageSource: require("fit-buddy/assets/HomeScreen/fbw-workout.jpg"),
-    // },
-    // {
-    //   type: "Yoga",
-    //   screen: "ThemedLevels",
-    //   imageSource: require("fit-buddy/assets/HomeScreen/yoga-workout.png"),
-    // },
   ];
 
-  // const handleDifficultyPress = (workoutDifficulty, workoutType) => {
-  //   // Small fix for the route parameters for the EasyLevels
-  //   // TypeError: Cannot read property 'levelCompleted' of undefined
-  //   navigation.navigate("ExerciseScreen", {
-  //     workoutDifficulty,
-  //     workoutType,
-  //   });
-  // };
-
+  // What information will be passed after user selects the difficulty
   const handleDifficultyPress = (workoutType, workoutDifficulty) => {
-    // const exerciseList =
-    //   exerciseData[String(workoutType)][String(workoutDifficulty)];
-    // console.log(exerciseList);
-    // console.log("workoutType:", String(workoutType));
-    // console.log("workoutDifficulty:", String(workoutDifficulty));
-    // console.log(exerciseData["Legs"]["Easy1"]);
-
     navigation.navigate("ExerciseScreen", {
-      // exerciseList,
       workoutDifficulty,
       workoutType,
     });
   };
 
+  // Every difficulty make a bigger gap
   const renderGapView = (index) => {
     if (index > 0 && index % 2 === 0) {
       return <View style={styles.gapView} />;
@@ -104,11 +57,13 @@ const SelectedWorkoutScreen = ({ navigation, route }) => {
       <ScrollView style={{ width: "100%" }}>
         {/* Display all workout types */}
         <View style={styles.workoutDifficultiesContainer}>
+          {/* Here is a small placeholder for the FBW workout type - it displays that the page is building */}
           {type === "FBW" ? (
             <View style={styles.centeredContent}>
               <Text style={styles.pageInBuildingText}>Page in building</Text>
             </View>
           ) : (
+            // Styling and content for all other workout types
             workoutDifficulties.map((workout, index) => (
               <React.Fragment key={index}>
                 <TouchableOpacity
@@ -117,12 +72,6 @@ const SelectedWorkoutScreen = ({ navigation, route }) => {
                     handleDifficultyPress(type, workout.difficulty)
                   }
                 >
-                  {/* <ImageBackground
-                  source={workout.imageSource}
-                  style={styles.image}
-                >
-                  <View style={styles.darkOverlay} />
-                </ImageBackground> */}
                   <Text style={styles.difficultyText}>
                     {workout.difficulty}
                   </Text>
@@ -141,7 +90,6 @@ const SelectedWorkoutScreen = ({ navigation, route }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // justifyContent: "center",
     alignItems: "center",
     backgroundColor: "rgba(40, 44, 46,1)",
   },
@@ -153,81 +101,31 @@ const styles = StyleSheet.create({
     width: "90%",
     height: 100,
     borderRadius: 10,
-    // margin: 10,
     justifyContent: "center",
-    alignItems: "center",
     alignSelf: "center",
     marginVertical: 5,
-    // borderBottomWidth: 12,
-    // borderLeftWidth: 12,
   },
   difficultyText: {
     fontFamily: "TitleFontBold",
-    // fontWeight: "bold",
     fontSize: 30,
     left: 20,
     color: "white",
     alignSelf: "flex-start",
   },
-  placeholderText: {
-    fontFamily: "TitleFontBold",
-    // fontWeight: "bold",
-    fontSize: 30,
-    left: 20,
-    color: "white",
-  },
   centeredContent: {
-    // flex: 1,
     marginTop: 200,
     justifyContent: "center",
     alignSelf: "center",
-    // height: "100%",
   },
-
   pageInBuildingText: {
     fontSize: 18,
-    color: "black",
     fontFamily: "TitleFontBold",
-    // fontWeight: "bold",
     fontSize: 30,
     color: "white",
   },
-  image: {
-    position: "absolute",
-    // top: 15,
-    // left: 10,
-    width: "100%",
-    height: "100%",
-    // marginRight: 10,
-    alignSelf: "flex-start",
-    borderRadius: 20,
-    flex: 1,
-    resizeMode: "cover",
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: 10, // Adjust this value to set the desired border radius
-    overflow: "hidden", // This is important to ensure the border radius is applied correctly
-  },
-  textContainer: {
-    marginTop: 30,
-    padding: 20,
-    width: "100%",
-
-    // backgroundColor: "green",
-  },
-  titleText: {
-    textAlign: "center",
-    fontSize: 35,
-    fontFamily: "TitleFont",
-    color: "white",
-  },
-  darkOverlay: {
-    ...StyleSheet.absoluteFillObject,
-    backgroundColor: "rgba(0,0,0,0.5)", // Adjust the opacity (last value) to control darkness
-  },
   gapView: {
-    width: "45%", // Adjust this value to set the width of the gap
-    height: 20, // Adjust this value to control the height of the gap
+    width: "45%",
+    height: 20,
   },
 });
 
