2e82e568bd102b16650d4c3af44ab4abe72957d9
Dajuuu <40289712+Dajuuu@users.noreply.github.com>
2023-08-18 12:20:39 +0200

    CustomHeader comments

--- CustomHeader.js
+++ CustomHeader.js
@@ -1,39 +1,23 @@
-import React, { useState, useContext, useEffect } from "react";
+import React, { useState } from "react";
 import {
   View,
   Text,
   TouchableOpacity,
   StyleSheet,
   Dimensions,
-  Image,
   Modal,
-  ScrollView,
 } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import Icon from "react-native-vector-icons/FontAwesome5";
-// import SettingsOverlay from "./SettingsOverlay";
-// import { CreditsContext } from "./CreditsContext";
-import { Asset } from "expo-asset";
-
-// import { PointsContext } from "./PointsContext";
 
 // Get the height of the device
 const windowHeight = Dimensions.get("window").height;
 
 const CustomHeader = ({ title }) => {
-  // Cache the credits icon
-  // useEffect(() => {
-  //   const cacheIcon = async () => {
-  //     await Asset.fromModule(require("./assets/credits.png")).downloadAsync();
-  //   };
-  //   cacheIcon();
-  // }, []);
-
+  // Access the Navigation
   const navigation = useNavigation();
   // handles the settings overlay
   const [settingsVisible, setSettingsVisible] = useState(false);
-  // Import credits info
-  // const { credits } = useContext(CreditsContext);
 
   // Handle the settings overlay
   const handleSettingsButtonPress = () => {
@@ -43,11 +27,10 @@ const CustomHeader = ({ title }) => {
   const handleCloseSettings = () => {
     setSettingsVisible(false);
   };
-  /* // Because on the Android status bar is shown, I want to make a small
-      adjustment // to make sure that the status bar is not colliding with
-      anything */
+
   return (
     <View style={styles.header}>
+      {/* Declare the type of modal - needed for settings overlay */}
       <Modal
         visible={settingsVisible}
         animationType="slide"
@@ -75,47 +58,36 @@ const CustomHeader = ({ title }) => {
             </Text>
             <Text style={styles.overlayText}>Good luck!</Text>
 
-            <View style={styles.buttonContainer}>
-              <TouchableOpacity
-                style={styles.buttonCancel}
-                onPress={handleCloseSettings}
-              >
-                <Text style={styles.buttonText}>Hide</Text>
-              </TouchableOpacity>
-            </View>
+            {/* Hide button */}
+            <TouchableOpacity
+              style={styles.buttonCancel}
+              onPress={handleCloseSettings}
+            >
+              <Text style={styles.buttonText}>Hide</Text>
+            </TouchableOpacity>
           </View>
         </View>
       </Modal>
+
+      {/* Header UI */}
       {/* Icon on the left (go back) */}
       <TouchableOpacity
-        style={[styles.leftButton, styles.button, { marginRight: 10 }]}
+        style={styles.buttonsSpacing}
         onPress={() => navigation.goBack()}
       >
-        <Icon name="arrow-left" style={[styles.buttonIcon]} />
+        <Icon name="arrow-left" style={styles.buttonIcon} />
       </TouchableOpacity>
 
-      {/* Icon next to the one on the left (settings) */}
-
+      {/* Icon on the right (info) */}
       <View style={styles.titleContainer}>
         <Text style={styles.title}>{title}</Text>
       </View>
-      {/* Display the credits on the right */}
-      {/* <Image
-          source={require("./assets/credits.png")}
-          style={styles.creditsImage}
-        /> */}
-      {/* <Text style={styles.creditsText}>{credits}</Text> */}
-      {/* <Text style={styles.creditsText}>300000</Text> */}
+
       <TouchableOpacity
-        style={[styles.leftButton, styles.button]}
+        style={styles.buttonsSpacing}
         onPress={handleSettingsButtonPress}
       >
-        {/* <SettingsOverlay
-          visible={settingsVisible}
-          onClose={handleCloseSettings}
-        /> */}
-        {/* TODO add info overlay of some sort */}
-        <Icon name="info-circle" style={[styles.buttonIcon]} />
+        <Icon name="info-circle" style={styles.buttonIcon} />
       </TouchableOpacity>
     </View>
   );
@@ -126,34 +98,12 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "flex-end",
     height: windowHeight / 11,
-    // backgroundColor: "#f7d7ba",
     paddingHorizontal: 20,
-    // borderBottomWidth: 1,
-    // borderBottomColor: "#ccc",
     marginTop: 20,
-    // marginBottom: 10,
     paddingBottom: 15,
   },
-  leftButton: {
-    // marginRight: 10,
-    // padding: 12,
+  buttonsSpacing: {
     paddingHorizontal: 14,
-    // backgroundColor: "#ebb381",
-    // borderRadius: 20,
-  },
-  // rightButton: {
-  //   marginLeft: 10,
-  //   padding: 12,
-  //   backgroundColor: "#ebb381",
-  //   borderRadius: 20,
-  // },
-  button: {
-    // borderWidth: 1,
-    shadowColor: "rgba(0,0,0, .4)", // IOS
-    shadowOffset: { height: 1 }, // IOS
-    shadowOpacity: 1, // IOS
-    shadowRadius: 1, //IOS
-    elevation: 5, // Android
   },
   buttonText: {
     fontSize: 16,
@@ -169,38 +119,12 @@ const styles = StyleSheet.create({
     textAlign: "center",
     color: "white",
     fontFamily: "TitleFontBold",
-    // paddingBottom: 10,
   },
   titleContainer: {
     flex: 1,
-    flexDirection: "row", // Arrange the title and credits in a row
-    justifyContent: "space-between", // Space them evenly along the row
-    alignItems: "center", // Center them vertically within the header
-  },
-  creditsText: {
-    fontSize: 16,
-    fontWeight: "bold",
-    flexWrap: "wrap",
-    maxWidth: "70%",
-    alignSelf: "center",
-  },
-  creditsContainer: {
-    backgroundColor: "#ebb381",
-    paddingHorizontal: 12,
-    paddingVertical: 6,
-    borderRadius: 20,
     flexDirection: "row",
-    // justifyContent: "space-between", // Space them evenly along the row
-    alignItems: "center", // Center them vertically within the header
-    maxWidth: 110,
-    minWidth: 95,
-    maxHeight: 45,
-  },
-  creditsImage: {
-    width: 30,
-    height: 30,
-    marginRight: 5,
-    marginLeft: -5,
+    justifyContent: "space-between",
+    alignItems: "center",
   },
   overlay: {
     flex: 1,
@@ -214,7 +138,6 @@ const styles = StyleSheet.create({
     paddingVertical: 20,
     borderRadius: 8,
     width: "85%",
-    // maxHeight: "75%",
   },
   overlayTextTitle: {
     fontSize: 25,
@@ -229,23 +152,13 @@ const styles = StyleSheet.create({
     textAlign: "center",
     color: "white",
     fontFamily: "TitleFont",
-    // paddingVertical: 2,
     lineHeight: 24,
-  },
-
-  buttonYes: {
-    backgroundColor: "rgba(56,157,60,1)",
-    // paddingHorizontal: 20,
-    paddingVertical: 10,
-    borderRadius: 8,
-    width: 100,
   },
   buttonCancel: {
     backgroundColor: "rgba(56,64,56,1)",
     paddingHorizontal: 30,
     paddingVertical: 10,
     borderRadius: 8,
-    // width: 100,
     alignSelf: "center",
   },
   buttonText: {
